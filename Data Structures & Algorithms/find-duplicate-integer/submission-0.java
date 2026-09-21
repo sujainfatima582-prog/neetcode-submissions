485167442cfb5665443bfb098e721f05fbf0660c
sujainfatima582-prog <sujainfatima582@gmail.com>
2026-09-21 12:37:42 +0530

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+
+        int slow = 0, fast = 0;
+        while(true){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if(slow == fast){
+                break;
+            }
+        }
+         fast = 0;
+        while(true){
+            slow = nums[slow];
+            fast = nums[fast];
+
+            if(slow == fast){
+                return slow;
+            }
+        }
+        
+    }
+}
